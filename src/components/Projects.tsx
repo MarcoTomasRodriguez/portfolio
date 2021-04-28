6552a015dfaccb0d2d2c0d55db8dcb9024583c8e
Marco Tomás Rodríguez <marcotomasrodriguez@gmail.com>
2021-04-28 12:46:56 -0300

    Added repositoryUrl and websiteUrl to projects.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,8 +2,9 @@ import { useTranslation } from "react-i18next";
 
 type Project = {
   title: string;
-  repository: string;
   description: string;
+  websiteUrl?: string;
+  repositoryUrl?: string;
 };
 
 type ProjectsProps = {
@@ -19,9 +20,32 @@ export default function Projects({ projects }: ProjectsProps) {
       <div className="flex flex-col space-y-6">
         {projects &&
           projects.map((project, index) => (
-            <div key={index} className="flex flex-col p-5 shadow rounded">
+            <div
+              key={index}
+              className="flex flex-col p-5 shadow rounded space-y-2"
+            >
               <p className="text-black font-bold">{project.title}</p>
-              <p className="text-black mt-3 text-sm">{project.description}</p>
+              <p className="text-black text-sm">{project.description}</p>
+              <div className="flex flex-row text-sm space-x-4">
+                {project.websiteUrl && (
+                  <a
+                    href={project.websiteUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Website
+                  </a>
+                )}
+                {project.repositoryUrl && (
+                  <a
+                    href={project.repositoryUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Repository
+                  </a>
+                )}
+              </div>
             </div>
           ))}
       </div>
