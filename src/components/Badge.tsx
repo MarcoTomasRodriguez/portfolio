b201b3c101bbfe01004d7d7b440a58b11dc6ac85
Marco Tomás Rodríguez <marcotomasrodriguez@gmail.com>
2021-04-30 12:34:26 -0300

    Fixed PurgeCSS issue.

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,16 +1,30 @@
 import { ComponentProps } from "react";
+import classNames from "classnames";
 
 export type BadgeProps = ComponentProps<"div"> & {
   text: string;
-  color: "red" | "orange" | "amber" | "green" | "blue" | "gray";
+  color: "red" | "green" | "blue" | "gray";
 };
 
 export function Badge({ text, color = "blue", ...props }: BadgeProps) {
+  // Necessary for PurgeCSS.
+  const badgeClass = classNames(
+    "py-px",
+    "px-2",
+    "text-sm",
+    "font-medium",
+    "bg-opacity-20",
+    "rounded-xl",
+    {
+      "bg-red-500 text-red-900": color === "red",
+      "bg-green-500 text-green-900": color === "green",
+      "bg-blue-500 text-blue-900": color === "blue",
+      "bg-gray-500 text-gray-900": color === "gray",
+    }
+  );
+
   return (
-    <div
-      className={`py-px px-2 text-sm font-medium bg-opacity-20 rounded-xl bg-${color}-500 text-${color}-900`}
-      {...props}
-    >
+    <div className={badgeClass} {...props}>
       {text}
     </div>
   );
