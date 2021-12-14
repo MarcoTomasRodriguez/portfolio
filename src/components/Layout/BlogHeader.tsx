c891aa9aa44161a243f050b284bf5c309f8113bd
Marco Tomás Rodríguez <marcotomasrodriguez@gmail.com>
2021-12-14 08:32:32 -0300

    Added home redirect to blog header.

--- src/components/Layout/BlogHeader.tsx
+++ src/components/Layout/BlogHeader.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 const BlogHeader = () => {
   return (
     <header className="w-full z-50 bg-primary py-2 px-4 flex flex-row justify-between space-x-8 sm:space-x-6 md:space-x-7 lg:space-x-14">
-      <p className="flex flex-row text-white p-2">
-        Marco Tom&aacute;s Rodr&iacute;guez
-      </p>
+      <Link href="/articles" passHref>
+        <a className="flex flex-row text-white p-2">
+          Marco Tom&aacute;s Rodr&iacute;guez
+        </a>
+      </Link>
     </header>
   );
 };
