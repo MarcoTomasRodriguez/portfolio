00cf4c3f949656dc7d9a54b6dc64cf02fca386ae
Marco Tomás Rodríguez <marcotomasrodriguez@gmail.com>
2022-01-15 10:44:32 -0300

    Updated dependencies.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
 import { AppProps } from "next/app";
-import { useEffect } from "react";
-import { hotjar } from "react-hotjar";
 import { appWithTranslation } from "next-i18next";
 import nextI18NextConfig from "../../next-i18next.config.js";
 import "@styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    hotjar.initialize(2490551, 6);
-  }, []);
-
   return <Component {...pageProps} />;
 };
 
